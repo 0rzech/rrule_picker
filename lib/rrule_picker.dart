7491745171493ff1b9f801ada5fef97efbd57395
Piotr Orzechowski <piotr@orzechowski.tech>
2026-05-21 18:09:29 +0200

    Initial commit

---
+++ lib/rrule_picker.dart
@@ -0,0 +1,5 @@
+// Copyright 2026 Piotr Orzechowski
+// SPDX-License-Identifier: Apache-2.0
+
+export 'package:rrule_picker/localizations/localizations.dart';
+export 'package:rrule_picker/localizations/localizations_en.dart';
---
+++ test/rrule_picker_test.dart
@@ -0,0 +1,4 @@
+// Copyright 2026 Piotr Orzechowski
+// SPDX-License-Identifier: Apache-2.0
+
+void main() {}
